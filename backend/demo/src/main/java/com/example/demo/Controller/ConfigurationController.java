81f292ebc94527d0cd5c44179215b9a7e48fab80
yangninh <giang.voninh@hcmut.edu.vn>
2024-12-01 22:44:50 +0700

    Configuration

---
+++ backend/demo/src/main/java/com/example/demo/Controller/ConfigurationController.java
@@ -0,0 +1,80 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Exception.ResourceNotFoundException;
+import com.example.demo.Model.Configuration;
+import com.example.demo.Model.Report;
+import com.example.demo.Repository.ConfigurationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class ConfigurationController {
+    @Autowired
+    private ConfigurationRepository configurationRepository;
+
+    @GetMapping("/configurations")
+    public List<Configuration> getAllConfigurations(){
+        return configurationRepository.findAll();
+    }
+
+    @GetMapping("/configuration/{id}")
+    public ResponseEntity<Configuration> getConfiguration(@PathVariable long id){
+        Configuration configuration = configurationRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Configuration not exist with id :" + id));
+        return ResponseEntity.ok(configuration);
+    }
+
+    @PostMapping("/configuration")
+    public Configuration createConfiguration(@RequestBody Configuration configuration){
+        return configurationRepository.save(configuration);
+    }
+
+    @PutMapping("/configuration/{id}")
+    public ResponseEntity<Configuration> updateConfiguration(@PathVariable long id, @RequestBody Configuration configurationInfo) {
+        Configuration configuration = configurationRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Configuration not exist with id :" + id));
+
+        if(String.valueOf(configurationInfo.getGivenPage()) != null) {
+            configuration.setGivenPage(configurationInfo.getGivenPage());
+        }
+
+        if(String.valueOf(configurationInfo.getPageSize()) != null) {
+            configuration.setPageSize(configurationInfo.getPageSize());
+        }
+
+        if(configurationInfo.getpermittedFile() != null){
+            configuration.setpermittedFile(configurationInfo.getpermittedStringFile());
+        }
+
+
+
+        Configuration updatedConfiguration = configurationRepository.save(configuration);
+        return ResponseEntity.ok(updatedConfiguration);
+    }
+
+    @DeleteMapping("/configuration/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteConfigurationByID(@PathVariable long id){
+        Configuration configuration = configurationRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Configuration not exist with id :" + id));
+        configurationRepository.deleteById(id);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted configuration with id " + id, Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/configuration")
+    public ResponseEntity<Map<String, Boolean>> deleteAllConfigurations(){
+        configurationRepository.deleteAll();
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted all", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+
+}
---
+++ backend/demo/src/main/java/com/example/demo/Model/Configuration.java
@@ -0,0 +1,50 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Entity
+@Table(name = "configuration")
+public class Configuration {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "configuration_id")
+    private Long id;
+    @Column(name = "page_size")
+    private int pageSize;
+    @Column(name = "permitted_file")
+    private String permittedFile; // "pdf,word"
+
+    @Column(name = "given_page")
+    private int givenPage;
+
+    public List<String> getpermittedFile() {
+        return Arrays.asList(permittedFile.split(",")); // Chuyển chuỗi thành danh sách
+    }
+    public String getpermittedStringFile(){
+        return permittedFile;
+    }
+
+    public void setpermittedFile(String listString) {
+        permittedFile = listString;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getGivenPage() {
+        return givenPage;
+    }
+
+    public void setGivenPage(int givenPage) {
+        this.givenPage = givenPage;
+    }
+}
