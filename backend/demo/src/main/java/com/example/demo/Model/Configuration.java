d2433239540f895c5d4d4fd86f577f0971866485
subiuno123 <thinh.lam220304@hcmut.edu.vn>
2024-12-02 01:32:40 +0700

    Add @JsonIgnore and delete printer(Configuration)

--- backend/demo/src/main/java/com/example/demo/Model/Configuration.java
+++ backend/demo/src/main/java/com/example/demo/Model/Configuration.java
@@ -1,5 +1,6 @@
 package com.example.demo.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -19,8 +20,6 @@ public class Configuration {
     private String permittedFile; // "pdf,word"
     @Column(name = "given_page")
     private int givenPage;
-    @OneToOne(mappedBy = "config")
-    private Printer printer;
 
     public List<String> getpermittedFile() {
         return Arrays.asList(permittedFile.split(",")); // Chuyển chuỗi thành danh sách
@@ -47,14 +46,6 @@ public class Configuration {
 
     public void setGivenPage(int givenPage) {
         this.givenPage = givenPage;
-    }
-
-    public Printer getPrinter() {
-        return printer;
-    }
-
-    public void setPrinter(Printer printer) {
-        this.printer = printer;
     }
 
     public Long getId() {
