048b058eafbd17bc88e4889862a493b8ed4eadb9
HienLe20044 <hien.lengoc2004@hcmut.edu.vn>
2024-12-03 18:15:23 +0700

    Update management page

--- frontend/src/pages/SPSO/ManagementPage/ManagementPage.jsx
+++ frontend/src/pages/SPSO/ManagementPage/ManagementPage.jsx
@@ -1,12 +1,15 @@
 import { SPSOHeader } from "../../../components/SPSOHeader"
 import { Footer } from "../../../components/footer";
+import { DefaultA4Pages } from "./DefaultA4Pages";
+import { ValidDocuments } from "./ValidDocuments";
 
 export const ManagementPage = () => {
     return <>
         <div className="flex flex-col min-h-screen">
             <SPSOHeader />
-            <div className="flex flex-col flex-grow items-center justify-center w-full my-5">
-                <h1>ManagementPage</h1>
+            <div className="flex flex-col flex-grow items-center justify-center w-full my-5 gap-y-10">
+                <ValidDocuments/>
+                <DefaultA4Pages/>
             </div>
             <Footer />
         </div>
