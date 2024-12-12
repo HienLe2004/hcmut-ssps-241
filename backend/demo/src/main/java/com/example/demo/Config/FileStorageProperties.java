b2ed05d863a5b03aa086a2b5e90c6d5f1878f60b
quechivvt <166884387+quechihuhu@users.noreply.github.com>
2024-12-12 22:54:33 +0700

    Update FileStorageProperties.java

--- backend/demo/src/main/java/com/example/demo/Config/FileStorageProperties.java
+++ backend/demo/src/main/java/com/example/demo/Config/FileStorageProperties.java
@@ -14,6 +14,6 @@ public class FileStorageProperties {
     }
 
     public FileStorageProperties(){
-        uploadDir = "frontend/public/uploads";
+        uploadDir = "../../frontend/public/uploads";
     }
 }
